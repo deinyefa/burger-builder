8d8f818d7ea450f5a958f8a8e6884876d0ce177e
Deinyefa <deinye@webdrive.ca>
2019-05-22 08:53:50 -0400

    Create React App 2.0 (and up) support CSS Modules out of the box - no need to eject

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import appStyles from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={appStyles.App}>
+      <header className={appStyles.AppHeader}>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={appStyles.AppLink}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
