d73416fcf55856f00c3ae4712be9ac8015200564
Deinyefa <df.eporwei@gmail.com>
2019-05-22 08:59:00 -0400

    cleaned up App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React from 'react';
-import appStyles from './App.module.css';
+// import appStyles from './App.module.css';
 
 function App() {
   return (
-    <div className={appStyles.App}>
-      <header className={appStyles.AppHeader}>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className={appStyles.AppLink}
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
     </div>
   );
 }
