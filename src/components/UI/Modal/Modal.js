7a2c6f73506e74006e775e1ffeeb78addf29c206
Deinyefa <df.eporwei@gmail.com>
2019-05-24 15:22:18 -0400

    refactor Modal.js to prevent unnecessary rerendering

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,22 +1,29 @@
-import React, {Fragment} from 'react';
+import React, { Fragment, Component } from 'react';
 
 import ModalStyles from './Modal.module.css';
 import Backdrop from '../Backdrop/Backdrop';
 
 
-const modal = props => {
-  return (
-    <Fragment>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={ModalStyles.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ? '1' : '0'
-      }}>
-      {props.children}
-    </div></Fragment>
-  )
+class Modal extends Component {
+  shouldComponentUpdate = ( nextProps, nextState ) => {
+    return nextProps.show !== this.props.show;
+  }
+
+  render () {
+    return (
+      <Fragment>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={ModalStyles.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ? '1' : '0'
+          }}>
+          {this.props.children}
+        </div>
+      </Fragment>
+    )
+  }
 }
 
-export default modal;
+export default Modal;
