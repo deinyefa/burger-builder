8336678c905a4dc9b1c5fd0e575db10f1dc5ae6f
Deinyefa <df.eporwei@gmail.com>
2019-05-26 21:49:52 -0400

    fetching orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,18 +1,55 @@
 import React, { Component } from "react";
 
-import Order from '../../components/Orders/Order';
-import OrdersStyles from './Orders.module.css'
+import axios from "../../axios-orders";
+import { baseURL } from "../../constants/firebaseEnv";
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
+
+import Order from "../../components/Orders/Order";
+import OrdersStyles from "./Orders.module.css";
+
 
 class Orders extends Component {
+	state = {
+		orders: [],
+		loading: true,
+	};
+
+	componentDidMount = () => {
+		axios
+			.get(baseURL + "/orders.json")
+			.then(res => {
+				const fetchedOrders = [];
+				for (let key in res.data) {
+					fetchedOrders.push({ ...res.data[key], id: key });
+				}
+				this.setState({
+					loading: false,
+					orders: fetchedOrders,
+				});
+			})
+			.catch(err => this.setState({ loading: false }));
+	};
+
+	displayOrders = () => {
+		this.state.orders.map(order => {
+			return (
+				<Order
+					key={order.id}
+					ingredients={order.ingredients}
+					price={order.price}
+				/>
+			);
+		});
+	};
+
 	render() {
 		return (
 			<div className={OrdersStyles.Orders}>
 				<h1>Previous Orders</h1>
-                <Order />
-                <Order />
+				{/* {this.displayOrders} */}
 			</div>
 		);
 	}
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
